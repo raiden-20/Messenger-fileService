88cb2205200444cd79237168c8cf78cd489a2b97
undine_20 <lena_sheina@bk.ru>
2023-12-19 12:52:19 +0300

    fix name

--- src/main/java/ru/vsu/cs/sheina/fileservice/dto/UrlDTO.java
+++ src/main/java/ru/vsu/cs/sheina/fileservice/dto/UrlDTO.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.vsu.cs.sheina.fileservice.service.enums.FileSource;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 public class UrlDTO {
 
-    String sourseId;
+    String sourceId;
 
     String url;
 
